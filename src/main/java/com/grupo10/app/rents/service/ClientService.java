103d02b7c183b39343ce50cfe32061ceed71186a
IrmaRios19 <irma.rios.mt@usa.edu.co>
2022-10-17 22:28:59 -0500

    arreglo de client para que lea el update

--- src/main/java/com/grupo10/app/rents/service/ClientService.java
+++ src/main/java/com/grupo10/app/rents/service/ClientService.java
@@ -10,7 +10,10 @@ import com.grupo10.app.rents.entities.Quadbike;
 import com.grupo10.app.rents.interfaces.IClientRepository;
 import com.grupo10.app.rents.interfaces.IMessageRepository;
 import com.grupo10.app.rents.repository.ClientRepository;
+import java.lang.reflect.Field;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,7 +40,7 @@ public class ClientService {
     }
 
     public Client update(Client client) {
-        Client clientToUpdate = new Client();
+    /*    Client clientToUpdate = new Client();
 
         Optional<Client> currentClient = repository.findById(client.getIdClient());
         if (!currentClient.isEmpty()) {            
@@ -48,7 +51,35 @@ public class ClientService {
             clientToUpdate=repository.save(clientToUpdate);
             
         }
-        return clientToUpdate;
+        return clientToUpdate;*/
+    if (client.getIdClient() != null) {
+            Optional<Client> clientU = repository.findById(client.getIdClient());
+
+
+
+           if (!clientU.isEmpty()) {
+
+
+
+               for (Field f : client.getClass().getDeclaredFields()) {
+                    f.setAccessible(true);
+                    Object value;
+                    try {
+                        value = f.get(client);
+                        if (value != null) {
+                            System.out.println("entro");
+                            f.set(clientU.get(), value);
+                        }
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(CategoryService.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalAccessException ex) {
+                        Logger.getLogger(CategoryService.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            return repository.save(clientU.get());
+            }
+        return client;
     }
 
     public Boolean delete(Integer id) {
